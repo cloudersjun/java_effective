8abb30da0d2dca87a26c0e4d2ae90aa670d67624
cloudersjun <cloudersjun@gamail.com>
2017-05-25 18:43:42 +0800

    lll

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,9 @@ public class Main {
         List<Integer> l= new LinkedList();
         l.add(12);
         map.put("1",l);
-
+        String s="1";
+        String s2=s.toString();
+        s="3";
+        System.out.print(s2);
     }
 }
