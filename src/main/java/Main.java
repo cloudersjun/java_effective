f0cae29254a58b965b64643cf8c6f085ada55afa
cloudersjun <cloudersjun@gamail.com>
2017-05-25 15:12:25 +0800

    test commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+/**
+ * Created by lenovo on 2017/5/25.
+ */
+public class Main {
+    public static void main(String []args){
+        Map<String,List<Integer>> map=new HashMap<>();
+        List<Integer> l= new LinkedList();
+        l.add(12);
+        map.put("1",l);
+
+    }
+}
